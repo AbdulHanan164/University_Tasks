99883398700c04403ec836160d0eb15e6049a19f
AbdulHanan164 <hananmanan154@gmail.com>
2024-02-15 21:02:06 +0500

    Create 22F-3104_DSA_Lab7_Taskno4.cpp

---
+++ Data Structures/22F-3104_DSA_Lab7/22F-3104_DSA_Lab7_Taskno4.cpp
@@ -0,0 +1,26 @@
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//// Function to find and return the first uppercase character in a string
+//char first(string str) {
+//    for (int i = 0; i < str.length(); i++) {
+//        // check if the current character is a letter and is uppercase
+//        if (str[i] >= 'A' && str[i] <= 'Z') {
+//            return str[i]; // return the first uppercase letter found
+//        }
+//    }
+//    return 0; // return 0 if no uppercase letter was found
+//}
+//int main() {
+//    string str;
+//        cout << "Enter a string: ";
+//        cin >> str;
+//        char res = first(str);
+//        if (res == 0) {
+//            cout << "No uppercase letter" << endl;
+//        }
+//        else {
+//            cout << res << endl;
+//        }
+//    return 0;
+//}
