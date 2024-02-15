8cc024462fb26cce6bf96ef1eab61b0219144fb0
AbdulHanan164 <hananmanan154@gmail.com>
2024-02-15 21:02:00 +0500

    Create 22F-3104-DSA_Lab7_Taskno2.cpp

---
+++ Data Structures/22F-3104_DSA_Lab7/22F-3104-DSA_Lab7_Taskno2.cpp
@@ -0,0 +1,21 @@
+//#include <iostream>
+//using namespace std;
+//int GCD(int num1, int num2)// Function to calculate the Greatest Common Divisor
+//{
+//    if (num2 != 0)
+//        return GCD(num2, num1 % num2);// Recursive call with swapped arguments and remainder
+//    else
+//        return num1;// If n2 becomes 0 return n1 as the GCD
+//}
+//int main()
+//{
+//    int num1, num2;
+//    while (true) {
+//        cout << "Enter integer 1: ";
+//        cin >> num1;
+//        cout << "Enter integer 2: ";
+//        cin >> num2;
+//        cout << "Greatest Common Divisor of " << num1 << " and " << num2 << " is :" << GCD(num1, num2)<<endl;
+//    }
+//   return 0;
+//}
